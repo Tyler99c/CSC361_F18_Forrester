c2373371a8a039e838719d8f9b0ebdee5605eb77
Tyler F <tf0199@cs.ship.edu>
2018-10-31 14:44:44 -0400

    Started work on the main class and game screen

--- core/src/com/packtpub/libgdx/dangerdungeon/DangerDungeonMain.java
+++ core/src/com/packtpub/libgdx/dangerdungeon/DangerDungeonMain.java
@@ -3,20 +3,23 @@ package com.packtpub.libgdx.dangerdungeon;
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogix.gdx.graphics.GL20;
 
 public class DangerDungeonMain extends Game{
 
 	@Override
 	public void create() {
-		//Set Libgdx log level
+		//Set Libgdx lgo level to DEBUG
 		Gdx.app.setLogLevel(Application.LOG_DEBUG);
-		//Load assets
-		assets.instance.init(new AssetManger());
-		//Load prefrences for audio settings
-		GamePrefrences.instance.load();
-		AudioManager.instance.play(Assets.instance.music.song01);
-		// Start game at game screen... for now
-		setScreen(new GameScreen(this));
+		//Initialize controller and renderer
+		worldController = new WorldController();
+		worldRenderer = new WorldRenderer(worldController);
 	}
+	@Override public void render() {}
+	@Override public void resize(int width, int height) {}
+	@Override public void pause() {}
+	@Override public void resume() {}
+	@Override public void dispose() {}
+	
 
 }
