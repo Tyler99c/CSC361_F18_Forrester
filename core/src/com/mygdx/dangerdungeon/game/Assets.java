3e7ea355342234c299a15715238ed21e29e774b7
Unknown <tf0199@cs.ship.edu>
2018-11-05 21:05:35 -0500

    Updated Assets to get it working

--- core/src/com/mygdx/dangerdungeon/game/Assets.java
+++ core/src/com/mygdx/dangerdungeon/game/Assets.java
@@ -30,9 +30,62 @@ public class Assets implements Disposable, AssetErrorListener
 	
 	private AssetManager assetManager;
 	
-	public AssetFloor floor;
-	public AssetKnight knight;
-	public AssetBackground background;
+	public class AssetLevelDecoration
+	{
+		public final AtlasRegion floor;
+		public final AtlasRegion wall;
+		
+		public AssetLevelDecoration (TextureAtlas atlas)
+		{
+			wall = atlas.findRegion("Wall");
+			
+		}
+	}
 	
-	public class AssetFloor
+	//singleton: prevent instantiation form other classes
+	private Assets() {}
+	
+	public AssetPlayer player;
+	
+	public void init (AssetManager assetManager) 
+	{
+		this.assetManager = assetManager;
+		//set asset manager error handler
+		assetManager.setErrorListener(this);
+		// load texture atlas
+		assetManager.load(Constants.TEXTURE_ATLAS_OBJECTS,TextureAtlas.class);
+		//start loading assets and wait until finished
+		assetManager.finishLoading();
+		Gdx.app.degbu(TAG, "# of assets loaded: " + assetManager.finishLoading());
+		for (String a : assetManager.getAssetNames())
+			Gdx.app.debug(TAG, "asset: " + a);
+	}
+	
+	TextureAtlas atlas = assetManager.get(Constants.TEXTURE_ATLAS_OBJECTS);
+	
+	//Enable texture filtering for pixel smoothing
+	for (Texture t : atlas.getTextures())
+	{
+		t.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+	}
+	
+	//create game resource objects
+	player = new AssetBunny(atlas);
+	
+	@Override
+	public void dispose()
+	{
+		assetManager.dispose();
+	}
+	
+	public void error(String filename, Class type, Throwable throwable)
+	{
+		Gdx.app.error(TAG, "Couldn't load asset '" + filename + "'", (Exception)throwable);
+	}
+
+	@Override
+	public void error(AssetDescriptor asset, Throwable throwable) {
+		// TODO Auto-generated method stub
+		
+	}
 }
