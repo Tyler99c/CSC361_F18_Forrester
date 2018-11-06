4d92859d66a45b853d768e9246efe0ecf31d1e87
Unknown <tf0199@cs.ship.edu>
2018-11-05 20:05:41 -0500

    AbstractGame Screen

--- core/src/com/packtpub/libgdx/dangerdungeon/screens/AbstractGameScreen.java
+++ core/src/com/packtpub/libgdx/dangerdungeon/screens/AbstractGameScreen.java
@@ -1,11 +1,81 @@
 package com.packtpub.libgdx.dangerdungeon.screens;
 
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
-import com.packtpub.libgdx.canyonbunny.game.Assets;
+import com.mygdx.dangerdungeon.game.Assets;
+import com.mygdx.dangerdungeon.game.WorldController;
+import com.mygdx.dangerdungeon.game.WorldRenderer;
+import com.packtpub.libgdx.dangerdungeon.DangerDungeonMain;
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 
-public class AbstractGameScreen 
+
+public class AbstractGameScreen implements ApplicationListener
 {
+	private static final String TAG = DangerDungeonMain.class.getName();
+	
+	private WorldController worldController;
+	private WorldRenderer worldRenderer;
+	private boolean paused;
+	
+	public void init() { }
+	
+	public void update(float deltaTime) { }
+	
+	@Override public void create() 
+	{
+		//Set Libgdx log level to DEBUG
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
+		//Initialize controller and renderer
+		worldController = new WorldController();
+		worldRenderer = new WorldRenderer(worldController);
+		//Game world is active on start
+		paused = false;
+	}
 
+	@Override
+	public void resize(int width, int height) 
+	{
+		worldRenderer.resize(width, height);
+	}
+
+	@Override
+	public void render() 
+	{
+		//Do not game world when paused.
+		if (!paused ) {
+			//Update game world by the time that has passed
+			//since last rendererd frame.
+			worldController.update(Gdx.graphics.getDeltaTime());
+		}
+		//Sets the clear screen cloror to: Cornflower Blue
+		Gdx.gl.glClearColor(0x64/255.0f, 0x95/255.0f, 0xed/255.0f, 0xff/255.0f);
+		//Clears the screen
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		//Render game world to screen
+		worldRenderer.render();
+	}
+
+	@Override
+	public void pause() 
+	{
+		paused = true;
+	}
+
+	@Override
+	public void resume() 
+	{
+		paused = false;
+	}
+
+	@Override
+	public void dispose() 
+	{
+		worldRenderer.dispose();
+		
+	}
 }
